9d6d68cae5e67fe4c90674d6b95969d0fb1afe81
Kang <rkdalsgur032@unist.ac.kr>
2023-01-09 16:18:21 +0900

    2023-01-09

--- 패스트캠퍼스/코딩테스트 문제풀이/알고리즘/03/★Z_1074.py
+++ 패스트캠퍼스/코딩테스트 문제풀이/알고리즘/03/★Z_1074.py
@@ -20,9 +20,17 @@ def search (s1,s2,e1,e2):
         count += 4
     else:
         offset = ((e1 - s1) + 1) // 2
-        search(s1,        s2,        e1-offset, e2-offset)
-        search(s1,        s2+offset, e1-offset, e2)
-        search(s1+offset, s2,        e1,        e2-offset)
-        search(s1+offset, s2+offset, e1,        e2)
+        nexts = [
+            [s1,        s2,        e1-offset, e2-offset],
+            [s1,        s2+offset, e1-offset, e2],
+            [s1+offset, s2,        e1,        e2-offset],
+            [s1+offset, s2+offset, e1,        e2]
+        ]
+        for target in nexts:
+            next_s1, next_s2, next_e1, next_e2 = target
+            if (next_s1 <= r and r <= next_e1 and next_s2 <= c and c <= next_e2):
+                search(next_s1, next_s2, next_e1, next_e2)
+            else:
+                count += (next_e1 - next_s1 + 1) ** 2
 
-search (0,0,length-1, length-1)
+search(0,0,length-1, length-1)
