c582ad3cc1647cb669c4a087ec4ad2a98ff91ec6
Kang <rkdalsgur032@unist.ac.kr>
2023-01-11 16:42:07 +0900

    2023-01-11

--- 패스트캠퍼스/코딩테스트 문제풀이/자료구조/09/★강의실_1374.py
+++ 패스트캠퍼스/코딩테스트 문제풀이/자료구조/09/★강의실_1374.py
@@ -1,41 +1,25 @@
 import sys
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
 n = int(input())
-lecs = []
-time = 0
-count = 0
-
+answer = 1
+lectures = []
 for _ in range(n):
     num, start, end = map(int, input().split())
-    lecs.append((start, end))
+    heappush(lectures, (start, end))
 
-while lecs :
-    is_full = True
-    
-    for lec in lecs:
-        if lec[0] >= time: 
-            is_full = False 
-            break
-    if is_full : 
-        time = 0
-        count += 1
-    
-    min_num = 10000000000
-    target_i = -1
-    
-    for i, lec in enumerate(lecs):
-        if min_num > lec[0] and time < lec[0]:
-            min_num = lec[0]
-            target_i = i
-    
-    _, time = lecs[target_i]
-    lecs[target_i]
-    
-count += 1
+room = []
+heappush(room, heappop(lectures)[1])
 
-print(count)
-    
-    
-
-    
+while lectures:
+    end = heappop(room)
+    new_start, new_end = heappop(lectures)
+    if end > new_start:
+        heappush(room, end)
+        heappush(room, new_end)
+        answer += 1
+    else:
+        heappush(room, new_end)
+        
+print(answer) 
--- 패스트캠퍼스/코딩테스트 문제풀이/자료구조/09/★중앙값 구하기_2696.py
+++ 패스트캠퍼스/코딩테스트 문제풀이/자료구조/09/★중앙값 구하기_2696.py
@@ -1,21 +1,38 @@
-from heapq import heappop, heappush, nsmallest
 import sys
+from heapq import heappop, heappush
 input = sys.stdin.readline
 
 T = int(input())
 
 for _ in range(T):
-    m = int(input())
-    heap = []
     answer = []
+    max_heap = []
+    mid_val = 0
+    min_heap = []
+    
+    m = int(input())
     data = []
-    for i in range((m // 10)+1): data += list(map(int, input().split()))
-    for i ,num in enumerate(data):
-        heappush(heap, num)
-        if (i+1) % 2: # odd
-            mid_index = (i+1) // 2
-            answer.append(str(nsmallest(mid_index+1, heap)[-1]))
+    for _ in range(m//10 + 1):
+        data += list(map(int, input().split()))
+    mid_val = data[0]
+    answer.append(mid_val)
+    
+    for i in range(1, len(data)):
+        num = data[i]
+        if num <= mid_val: heappush(max_heap, (-num, num))
+        else: heappush(min_heap, (num, num))
+        if i % 2 == 0:
+            while len(max_heap) > len(min_heap):
+                heappush(min_heap, (mid_val, mid_val))
+                mid_val = heappop(max_heap)[1]
+            while len(min_heap) > len(max_heap):
+                heappush(max_heap, (-mid_val, mid_val))
+                mid_val = heappop(min_heap)[1]
+            answer.append(mid_val)
     print(len(answer))
-    for i, num in enumerate(answer):
+    for i , num in enumerate(answer):
         print(num, end=" ")
-        if i % 10 == 9: print("")
+        if i % 10 == 9: 
+            print("")
+        
+            
