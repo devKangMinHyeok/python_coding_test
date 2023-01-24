1ab27582159b5f24f0d2c6b9c188ee1cb9b4d705
Kang <rkdalsgur032@unist.ac.kr>
2023-01-24 12:32:41 +0900

    2023-01-24

--- 패스트캠퍼스/코딩테스트 문제풀이/알고리즘/06/★★중량제한_1939.py
+++ 패스트캠퍼스/코딩테스트 문제풀이/알고리즘/06/★★중량제한_1939.py
@@ -3,39 +3,41 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = [[] for i in range(n+1)]
-high = -1
-low = 1000000000
-
+graph = [[] for _ in range(n+1)]
+max_val = 0
+min_val = 100000000000
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b,c))
     graph[b].append((a,c))
-    low = min(low, c)
-    high = max(high, c)
+    max_val = max(max_val, c)
+    min_val = min(min_val, c)
 
 start, end = map(int, input().split())
-answer = low
 
-def bfs (pivot):
+low, high = min_val, max_val
+
+def bfs(mid):
     queue = deque([start])
     visit = [False] * (n+1)
     visit[start] = True
     while queue:
         target = queue.popleft()
-        for (node, w) in graph[target]:
-            if w >= pivot and not visit[node]:
-                queue.append(node)
-                visit[node] = True
+        for next_target, weight in graph[target]:
+            if weight >= mid and not visit[next_target]:
+                visit[next_target] = True
+                queue.append(next_target)
     return visit[end]
+                
+answer = 0
 
 while low <= high:
     mid = (low + high) // 2
-    if bfs(mid):
-        answer = mid
+    isAble = bfs(mid)
+    if isAble: 
         low = mid + 1
-    else:
-        high = mid - 1
+        answer = mid
+    else: high = mid - 1
 
 print(answer)
     
