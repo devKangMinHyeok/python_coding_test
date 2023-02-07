9f98b2271568eb2b2f78353a2188ede65c7bc2ab
Kang <rkdalsgur032@unist.ac.kr>
2023-02-07 12:01:02 +0900

    2023-02-07

--- 패스트캠퍼스/코딩테스트 문제풀이/알고리즘/17/좌표 압축_18870.py
+++ 패스트캠퍼스/코딩테스트 문제풀이/알고리즘/17/좌표 압축_18870.py
@@ -1,21 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+N = int(input())
 data = list(map(int, input().split()))
-data = [(data[i], i) for i in range(n)]
-data.sort()
-result = []
-cnt = 0
 
-for i in range(n-1):
-    result.append((cnt, data[i][1]))
-    if data[i][0] < data[i+1][0]: cnt += 1
+new_data = list(set(data))
+new_data.sort()
 
-result.append((cnt, data[-1][1]))
-result.sort(key = lambda x : x[1])
+table = {}
+for i, num in enumerate(new_data):
+    table[num] = i
 
-for i,(num, index) in enumerate(result):
-    if i < n-1: print(num, end=" ")
-    else: print(num)
-    
+for num in data:
+    print(table[num], end=" ")
